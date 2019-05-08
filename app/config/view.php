81b8650e0af1e95119727e2ae1b3228c50875c9f
Maxim Bondarev <macintosh.way@gmail.com>
2019-05-08 12:09:19 +0500

    enable cache

--- app/config/view.php
+++ app/config/view.php
@@ -2,6 +2,6 @@
 
 return [
     'view.templates' => $_SERVER['DOCUMENT_ROOT'].'/app/views',
-    'view.cachex' => $_SERVER['DOCUMENT_ROOT'].'/cache/views',
+    'view.cache' => $_SERVER['DOCUMENT_ROOT'].'/cache/views',
     'view.engine' => 'twig',
 ];
