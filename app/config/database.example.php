27bc605d2b02e4ce00f2ce6b29d24b3695d2e551
Maxim Bondarev <macintosh.way@gmail.com>
2019-05-11 11:01:45 +0500

    hide database config

---
+++ app/config/database.example.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'username' => 'homestead',
+    'password' => 'secret',
+    'database' => 'homestead',
+    'host' => 'localhost',
+    'driver' => 'mysql',
+];
--- app/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'username' => 'mueble',
-    'password' => 'secret',
-    'database' => 'mueble',
-    'host' => 'mueble-mariadb',
-    'driver' => 'mysql',
-];
